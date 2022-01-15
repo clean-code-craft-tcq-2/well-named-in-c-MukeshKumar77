f905b1f68453a09033b58eb2d280a5a5db22a080
Mukesh <90908610+MukeshKumar77@users.noreply.github.com>
2022-01-15 18:55:34 +0530

    Create ColorPairVariables.c
    separate global variables

---
+++ ColorPairVariables.c
@@ -0,0 +1,11 @@
+static const int MAX_COLORPAIR_NAME_CHARS = 16;
+
+static const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+static const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
+static int numberOfMinorColors =
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
